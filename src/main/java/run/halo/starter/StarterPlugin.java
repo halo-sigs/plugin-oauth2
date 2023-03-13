eaf337ef487c2eb65cc69ee39360875479372f07
Ryan Wang <i@ryanc.cc>
2023-03-13 12:33:12 +0800

    Initial commit

---
+++ src/main/java/run/halo/starter/StarterPlugin.java
@@ -0,0 +1,29 @@
+package run.halo.starter;
+
+import org.pf4j.PluginWrapper;
+import org.springframework.stereotype.Component;
+import run.halo.app.extension.Scheme;
+import run.halo.app.extension.SchemeManager;
+import run.halo.app.plugin.BasePlugin;
+
+/**
+ * @author guqing
+ * @since 2.0.0
+ */
+@Component
+public class StarterPlugin extends BasePlugin {
+
+    public StarterPlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("插件启动成功！");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("插件停止！");
+    }
+}
