aba33f95e2bd4c1b71a14e85466d5aded8bdde77
guqing <i@guqing.email>
2024-07-01 12:46:25 +0800

    feat: adaptive device management for halo 2.17

--- src/test/java/run/halo/oauth/Oauth2LoginConfigurationTest.java
+++ src/test/java/run/halo/oauth/Oauth2LoginConfigurationTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 import run.halo.app.extension.ReactiveExtensionClient;
+import run.halo.app.security.LoginHandlerEnhancer;
 
 /**
  * Tests for {@link Oauth2LoginConfiguration}.
@@ -17,8 +18,9 @@ class Oauth2LoginConfigurationTest {
     @Test
     void constructor() {
         ReactiveExtensionClient extensionClient = mock(ReactiveExtensionClient.class);
+        var loginHandlerEnhancer = mock(LoginHandlerEnhancer.class);
         Oauth2LoginConfiguration oauth2LoginConfiguration =
-            new Oauth2LoginConfiguration(extensionClient);
+            new Oauth2LoginConfiguration(extensionClient, loginHandlerEnhancer);
         assertNotNull(oauth2LoginConfiguration);
     }
 
