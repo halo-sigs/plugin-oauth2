c2186a4414edda193e632ab022d9bb5382ebab18
JohnNiang <johnniang@foxmail.com>
2024-09-29 12:30:10 +0800

    Remove unused code
    Signed-off-by: JohnNiang <johnniang@foxmail.com>

--- src/main/java/run/halo/oauth/HaloOAuth2AuthenticationWebFilter.java
+++ src/main/java/run/halo/oauth/HaloOAuth2AuthenticationWebFilter.java
@@ -1,10 +1,10 @@
 package run.halo.oauth;
 
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.oauth2.client.authentication.OAuth2LoginReactiveAuthenticationManager;
 import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.userinfo.DefaultReactiveOAuth2UserService;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationCodeAuthenticationTokenConverter;
+import org.springframework.security.oauth2.client.web.server.authentication.OAuth2LoginAuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationFailureHandler;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
@@ -13,28 +13,27 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-import run.halo.app.core.user.service.UserConnectionService;
 import run.halo.app.security.AuthenticationSecurityWebFilter;
 
+/**
+ * OAuth2 authentication web filter.
+ *
+ * @author johnniang
+ * @since 2.20.0
+ */
 @Component
 public class HaloOAuth2AuthenticationWebFilter implements AuthenticationSecurityWebFilter {
 
     private final WebFilter delegate;
 
     public HaloOAuth2AuthenticationWebFilter(Oauth2LoginConfiguration configuration,
-        ServerSecurityContextRepository securityContextRepository,
-        UserConnectionService connectionService,
-        ReactiveUserDetailsService userDetailsService) {
+        ServerSecurityContextRepository securityContextRepository) {
         var authManager = new OAuth2LoginReactiveAuthenticationManager(
             new WebClientReactiveAuthorizationCodeTokenResponseClient(),
             new DefaultReactiveOAuth2UserService()
         );
-        var filter = new HaloOAuth2LoginAuthenticationWebFilter(
-            authManager,
-            configuration.getAuthorizedClientRepository(),
-            connectionService,
-            userDetailsService
-        );
+        var filter = new OAuth2LoginAuthenticationWebFilter(authManager,
+            configuration.getAuthorizedClientRepository());
         filter.setRequiresAuthenticationMatcher(configuration.getAuthenticationMatcher());
         var converter = new ServerOAuth2AuthorizationCodeAuthenticationTokenConverter(
             configuration.getClientRegistrationRepository()
@@ -43,7 +42,7 @@ public class HaloOAuth2AuthenticationWebFilter implements AuthenticationSecurity
             new RedirectServerAuthenticationSuccessHandler("/uc")
         );
         filter.setAuthenticationFailureHandler(
-            new RedirectServerAuthenticationFailureHandler("/console/login?error")
+            new RedirectServerAuthenticationFailureHandler("/login?oauth2_error")
         );
         filter.setServerAuthenticationConverter(converter);
         filter.setSecurityContextRepository(securityContextRepository);
